551b2e90bca0c26d6ca46a4111473f7d4f7c0412
AnaM93 <ana.18_03@yahoo.com>
2024-01-04 13:46:26 -0800

    added more js to the project

--- script.js
+++ script.js
@@ -39,14 +39,28 @@ function isCorrect (guessString) {
 
 
 // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
-    // TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
-
-
+for (let btn of optionButtons) {
+    btn.addEventListener("click" , function (event) {
+// TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
+        explanation.textContent = fact.explanation;
 // TODO 7: Within the event handler function, 
 // Use a for loop to disable all the option buttons
-
-
+            for (let btns of optionButtons) {
+                disable(btns)
+            } 
+ 
 // TODO 8: Within the event handler function,
 // Get the guessed value from the clicked button
+let guessedValue = btn.getAttribute("value")
+
 // Use a conditional to compare the guess to the fact's answer
 // and add the "correct"/"incorrect" class as appropriate
+
+if(isCorrect(guessedValue)) {
+    btn.classList.add("correct")
+} else {
+    btn.classList.add("incorrect")
+}
+
+})
+}
