3bf1b86d2876ece7ff332e04ae1be311f46bc762
AnaM93 <ana.18_03@yahoo.com>
2024-01-05 13:29:20 -0800

    added more question to the Quiz

--- script.js
+++ script.js
@@ -1,66 +1,97 @@
 
-// TODO 1: Declare & assign variables pointing to the corresponding element(s)
-// statement should be the "statement" div
+
 const statement = document.getElementById("statement");
-// optionButtons should be all the elements within the "options" div
 const optionButtons = document.querySelector('#options').children;
-// explanation should be the "explanation" div
 const explanation = document.getElementById('explanation');
+const nextQuestionBtn = document.getElementById('sg_btn');
+const firstNumberScore = document.getElementById('firstScore');
+const secondNumberScore = document.getElementById('secondScore');
 
-
-// TODO 2: Declare & assign a variable called fact
-// Its value should be an object with a statement, true/false answer, and explanation 
-let fact = {
+let fact = [
+    {
 statement: "Type 'String'  is a primitive data type in JS",
 answer: true,
 explanation: "Type 'String' is one of the 7 primitive types in JS along: number, bigint, boolean, undefined, symbol and null"
+    },
 
-};
+   {
+statement: "1 + 1 === 2",
+answer: true,
+explanation: "The addition (+) operator produces the sum of numeric operands or string concatenation."
+    },
+
+    {
+statement: "Primitive values are mutable",
+answer: false,
+explanation: "Primitive values are immutable (they are hardcoded and cannot be changed)."
+    }
+
+];
 
 
-// TODO 3: Set the text of the statement element to the fact's statement
-statement.textContent = fact.statement;
-
-// TODO 4: Declare disable & enable functions to set or remove the "disabled" attribute from a given button element
-// disable(button) should set the button element's attribute "disabled" to the value ""
 function disable (button) {
     button.setAttribute("disabled", "")
 }
-// enable(button) should remove the attribute "disabled" from the button element
+
 function enable (button) {
     button.removeAttribute("disabled")
 }
 
-// TODO 5: Declare an isCorrect function that compares a guess to the right answer
-// isCorrect(guess) should return true if the guess matches the fact's answer
 function isCorrect (guessString) {
-    return guessString === fact.answer.toString()
+    return guessString === fact[i].answer.toString()
 }
 
+let i = 0;
+statement.textContent = fact[i].statement;
+explanation.classList.add("hide")
+disable(nextQuestionBtn)
 
-// TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
-for (let btn of optionButtons) {
-    btn.addEventListener("click" , function (event) {
-// TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
-        explanation.textContent = fact.explanation;
-// TODO 7: Within the event handler function, 
-// Use a for loop to disable all the option buttons
-            for (let btns of optionButtons) {
-                disable(btns)
-            } 
- 
-// TODO 8: Within the event handler function,
-// Get the guessed value from the clicked button
-let guessedValue = btn.getAttribute("value")
+        for (let btn of optionButtons) {
+            btn.addEventListener("click" , function (event) {
 
-// Use a conditional to compare the guess to the fact's answer
-// and add the "correct"/"incorrect" class as appropriate
+                explanation.classList.remove("hide")
+                explanation.classList.add("show")
+                explanation.textContent = fact[i].explanation;
+                    for (let btns of optionButtons) {
+                        disable(btns)
+                    } 
+        
+            let guessedValue = btn.getAttribute("value")
+        
+            if(isCorrect(guessedValue)) {
+                btn.classList.add("correct")
+                firstNumberScore.textContent = i + 1;
+            }   else {
+                btn.classList.add("incorrect")
+            }
+        
+            secondNumberScore.textContent = i+1;
 
-if(isCorrect(guessedValue)) {
-    btn.classList.add("correct")
-} else {
-    btn.classList.add("incorrect")
-}
+            if (i === fact.length-1) {
+                nextQuestionBtn.textContent = "No more questions!"
+                disable(nextQuestionBtn)
+            } else {
+                enable(nextQuestionBtn)
+            }
+            
 
+        })
+        }
+
+
+
+
+    nextQuestionBtn.addEventListener("click" , function () {
+    i++
+    statement.textContent = fact[i].statement;
+    explanation.classList.remove("show")
+    explanation.classList.add("hide")
+    for (let btns of optionButtons) {
+        enable(btns);
+        btns.classList.remove("correct", "incorrect");
+    } 
+    explanation.textContent = "";
+    
+    disable(nextQuestionBtn)
+    
 })
-}
